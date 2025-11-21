cf31e367f064a8b1b647c777295f6600f37440d9
a24caisu <a24caisu@student.his.se>
2025-11-21 13:43:58 +0100

    Player Animations
    Idle and Running animations
    + Script to switch

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    Animator PlayerAnimator;
+    void Start()
+    {
+        PlayerAnimator = GetComponent<Animator>(); // Ger access till Animatorn du skapat
+        PlayerAnimator.SetBool("Run", false);
+    }
+    
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("a") || Input.GetKeyDown("d"))
+        {
+            PlayerAnimator.SetBool("Run", true);
+        }
+        if(Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            PlayerAnimator.SetBool("Run", false);
+        }
+    }
+
+}
